cb675091a40f0618af7023837ac531f9b5c36b0c
Gregory Wagner <wagner.greg@gmail.com>
2020-05-06 23:56:16 -0400

    Uses plots in two gaussians example

--- examples/OneDWaveEquation/two_gaussians.jl
+++ examples/OneDWaveEquation/two_gaussians.jl
@@ -1,35 +1,36 @@
 # # Two Gaussians
 # 
-# In this example, a gaussian initial condition is let loose and two gaussians 
+# In this example, a Gaussian initial condition is let loose and two gaussians 
 # that propagate in either direction emerge.
 
 using
     OscillatoryFlows.OneDWaveEquation,
     Printf,
-    PyPlot
+    Plots
 
-Lx = 2π
-nx = 256
-c = 1
+# ## Build the problem
 
-problem = Problem(; nx=nx, Lx=Lx, c=c, beta=0, dt=0.01)
+problem = Problem(; nx=256, Lx=2π, c=1, beta=0, dt=0.01)
 
-ξ₀(x) = exp(-64x^2)
+nothing # hide
+
+# ## A Gaussian initial condition
+#
+# This initial condition has width √25 = 5
+
+ξ₀(x) = exp(-50x^2)
 
 set_ξ!(problem, ξ₀)
 
-close("all")
-fig, axs = subplots()
+# ## Run, and animate the results
 
-updatevars!(problem)
-plot(problem.grid.x, real.(problem.vars.ξ))
-
-for i = 1:100
+anim = @animate for i = 1:100
     stepforward!(problem, 1)
     updatevars!(problem)
-    cla()
-    plot(problem.grid.x, real.(problem.vars.ξ))
-    title(@sprintf("Two Gaussians, \$ t = %.2f \$", problem.clock.t))
-    ylim(-0.5, 1.1)
-    pause(0.1)
+
+    plot(problem.grid.x, problem.vars.ξ,
+         title = @sprintf("Two Gaussians, t = %.2f", problem.clock.t),
+         ylims = (-0.5, 1.1))
 end
+
+mp4(anim, "two_gaussians.mp4", fps=12)
