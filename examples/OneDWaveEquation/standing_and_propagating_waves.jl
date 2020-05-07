3d6c05ba88f466d647a9a51f7be784a4a9095f51
Gregory Wagner <wagner.greg@gmail.com>
2020-05-06 23:51:22 -0400

    Uses Plots rather than PyPlot in OneDWaveEqn example

--- examples/OneDWaveEquation/standing_and_propagating_waves.jl
+++ examples/OneDWaveEquation/standing_and_propagating_waves.jl
@@ -6,68 +6,88 @@
 using 
     OscillatoryFlows.OneDWaveEquation,
     Printf,
-    PyPlot
+    Plots
 
 # ## Parameters
+#
+# Here, we choose a domain size, numerical resolution,
+# wave phase speed, and the time-step.
 
 Lx = 2π     # domain length
 nx = 256    # number of grid points
  c = 1      # wave phase speed
 dt = 0.01   # time-step
 
-# ## Setting up the problems
-   standing_wave_problem = Problem(; nx=nx, Lx=Lx, c=c, beta=0, dt=dt)
-propagating_wave_problem = Problem(; nx=nx, Lx=Lx, c=c, beta=0, dt=dt)
+nothing # hide
+
+# ## Set up two problems
+#
+# We set up two problems: one to simulate a standing wave, and
+# another to simulate a propagating wave.
+
+   standing_problem = Problem(; nx=nx, Lx=Lx, c=c, beta=0, dt=dt)
+propagating_problem = Problem(; nx=nx, Lx=Lx, c=c, beta=0, dt=dt)
+
+nothing # hide
 
 # ## Initial conditions
-# ξ(x) = cos(k * x - k * c * t)
-# u(x) = k * c * sin(k * x - k * c * t)
+#
+# For our initial conditions we use a sinusoid with wavenumber=8.
+#
+# The initial velocity for the standing wave is zero.
+#
+# We determine the initial velocity for the propagating wave 
+# using the dispersion relation. For t > 0, the propagating
+# wave solution has the displacement
+#
+# $$ ξ(x, t) = \cos(k x - k c t) $$
+#
+# This implies that the velocity of the standing wave,
+# $u = ∂_t ξ$, is
+#
+# $$ u(x, t) = k * c * sin(k * x - k * c * t) $$
+#
+# Taking $t=0$ determines the initial conditions, 
+# $ξ(x, t=0)$ and $u(x, t=0)$.
+
+## Wavenumber
 k = 8
+
+## Function that define the initial condition
 ξ₀(x) = cos(k * x)
 u₀(x) = k * c * sin(k * x)
 
-# ### Standing wave initial condition
+## Set standing wave initial condition
+set_ξ!(standing_problem, ξ₀)
 
-set_ξ!(standing_wave_problem, ξ₀)
+## Set propagating wave initial condition
+set_ξ!(propagating_problem, ξ₀)
+set_u!(propagating_problem, u₀)
 
-# ### Propagating wave initial condition
+# ## Time-step and animate
+#
+# We're finally ready to time-step our problem forward.
+# Along the way, we create an animation to visualize the solution.
 
-set_ξ!(propagating_wave_problem, ξ₀)
-set_u!(propagating_wave_problem, u₀)
+anim = @animate for i = 1:100
+    stepforward!(standing_problem, 1)
+    stepforward!(propagating_problem, 1)
 
+    updatevars!(standing_problem)
+    updatevars!(propagating_problem)
 
-# ## Visualization
+    standing_plot = 
+        plot(standing_problem.grid.x, standing_problem.vars.ξ,
+             title = @sprintf("Standing wave, t = %.2f", standing_problem.clock.t),
+             xlabel = "x", ylabel = "s", ylims = (-1, 1))
+             
+    propagating_plot = 
+        plot(propagating_problem.grid.x, propagating_problem.vars.ξ,
+             title = @sprintf("Propagating wave, t = %.2f ", standing_problem.clock.t),
+             xlabel = "x", ylabel = "s", ylims = (-1, 1))
 
-close("all")
-fig, axs = subplots(nrows=2, sharey=true, sharex=true)
-
-function makeplot!(axs, standing_wave_problem, propagating_wave_problem)
-
-    updatevars!(standing_wave_problem)
-    updatevars!(propagating_wave_problem)
-    
-    sca(axs[1]); cla()
-    plot(standing_wave_problem.grid.x, real.(standing_wave_problem.vars.ξ))
-    title(@sprintf("Standing wave, \$ t = %.2f \$", standing_wave_problem.clock.t))
-
-    sca(axs[2]); cla()
-    plot(propagating_wave_problem.grid.x, real.(propagating_wave_problem.vars.ξ))
-    title(@sprintf("Propagating wave, \$ t = %.2f \$", propagating_wave_problem.clock.t))
-
-    ylim(-1.1, 1.1)
-    tight_layout()
-    pause(0.05)
-
-    return nothing
+    plot(standing_plot, propagating_plot, layout = (2, 1), legend = false)
 end
 
-makeplot!(axs, standing_wave_problem, propagating_wave_problem)
-
-# ## Time-step
-
-for i = 1:100
-    stepforward!(standing_wave_problem, 1)
-    stepforward!(propagating_wave_problem, 1)
-
-    makeplot!(axs, standing_wave_problem, propagating_wave_problem)
-end
+## Save the animation to disk as an mp4
+mp4(anim, "standing_propagating_waves.mp4", fps=12) # hide
