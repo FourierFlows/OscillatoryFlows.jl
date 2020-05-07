24554914c949e80a5af3e99730c97add1dab50e6
Gregory Wagner <wagner.greg@gmail.com>
2020-05-07 00:04:33 -0400

    Adds packages to docs env and main env, plus simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,12 @@ using
   FourierFlows,
   Test
 
+using OscillatoryFlows: OneDWaveEquation, OneDSurfaceWaves
+
 # the devices on which tests will run
 devices = (CPU(),)
-@has_cuda devices = (CPU(), GPU())
-@has_cuda using CuArrays
+#@has_cuda devices = (CPU(), GPU())
+#@has_cuda using CuArrays
 
 # Run tests
 testtime = @elapsed begin
@@ -13,9 +15,13 @@ testtime = @elapsed begin
 for dev in devices
   
   println("testing on "*string(typeof(dev)))
-  
-  @testset "empty testset" begin
-    @test 1==1
+ 
+  @testset "OneDWaveEquation" begin
+      @test typeof(OneDWaveEquation.Problem(nx=4)) <: FourierFlows.Problem
+  end
+
+  @testset "OneDSurfaceWaves" begin
+      @test typeof(OneDSurfaceWaves.Problem(nx=4)) <: OneDSurfaceWaves.Problem
   end
 end
 
